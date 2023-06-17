13988a7d4d4a85d217b5e23af055788ef12b4a20
Daniel Ogunleye <daniel.ogunleye@engie.com>
2023-06-17 19:07:47 +0100

    testing changes

--- app/src/main/java/com/example/movie/MainActivity.kt
+++ app/src/main/java/com/example/movie/MainActivity.kt
@@ -7,5 +7,9 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        println("Hello ")
+        println("Hello ")
+        println("Hello ")
     }
 }
