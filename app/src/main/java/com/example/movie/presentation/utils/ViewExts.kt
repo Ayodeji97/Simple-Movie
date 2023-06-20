d4adec518d0c0fecc1bae08f2301d273cd267487
Daniel Ogunleye <daniel.ogunleye@engie.com>
2023-06-20 10:59:06 +0100

    clean up import

--- app/src/main/java/com/example/movie/presentation/utils/ViewExts.kt
+++ app/src/main/java/com/example/movie/presentation/utils/ViewExts.kt
@@ -1,22 +1,15 @@
 package com.example.movie.presentation.utils
 
-import android.os.Build
 import android.os.CountDownTimer
-import android.provider.Settings.System.DATE_FORMAT
 import android.view.View
 import android.widget.ImageView
-import androidx.annotation.RequiresApi
 import androidx.appcompat.widget.SearchView
 import androidx.fragment.app.Fragment
-import com.bumptech.glide.Glide
 import com.example.movie.R
 import com.google.android.material.snackbar.Snackbar
 import com.squareup.picasso.Picasso
-import java.time.ZoneId
-import java.time.ZonedDateTime
-import java.time.format.DateTimeFormatter
 
-private const val DATE_FORMATTER = "yyyy-MMM-dd"
+
 
 fun ImageView.loadImage(url: String, placeHolder: Int = R.drawable.vikings) {
     Picasso.get().load(url).placeholder(placeHolder).into(this)
